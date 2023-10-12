fa7b45de46ee6e1ac05033a58edae5e76bbc92f3
tanukile <backpacker320@gmail.com>
2023-10-12 17:46:46 +0800

    設定檔更新

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const jsonServer = require('json-server'); // importing json-server library
-const server = jsonServer.create();
+const jsonServer = require('json-server');
+const auth = require('json-server-auth');
+const app = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3001; // you can use any port number here; i chose to use 3001
+
+app.db = router.db;
 
 server.use(middlewares);
+server.use(auth);
 server.use(router);
-
-server.listen(port);
+server.listen(3000, () => {
+  console.log('JSON Server is running');
+});
