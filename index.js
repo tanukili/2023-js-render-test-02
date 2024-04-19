68693695f05b16dede3e21e009ef65f586ed4821
tanukile <backpacker320@gmail.com>
2024-04-19 17:09:02 +0800

    feat: 跨域請求

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ app.db = router.db;
 
 app.use(middlewares);
 app.use(auth);
+
+// 跨域請求
+module.exports = (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+};
+
 app.use(router);
 app.listen(3000, () => {
   console.log('JSON Server is running');
